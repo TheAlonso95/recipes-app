e9cf29bcde67638c9e2443f53896c136ec933e51
Alonso <rafael.a.m95@hotmail.com>
2021-09-24 00:55:36 +0000

    Test commit

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 
+// test
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
